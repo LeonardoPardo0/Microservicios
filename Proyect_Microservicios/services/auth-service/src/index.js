ec07d93fe21413787587cdc313c6a877d0f70dca
LeonardoPardo0 <leonardopardo2077@gmail.com>
2025-10-31 17:49:07 -0500

    Add files via upload

---
+++ Proyect_Microservicios/services/auth-service/src/index.js
@@ -0,0 +1,33 @@
+﻿/**
+ * Auth Service
+ * Servicio de autenticación: registro, login y validación JWT
+ */
+
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/auth.routes.js";
+
+dotenv.config();
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+
+// Conexión a MongoDB
+connectDB();
+
+// Rutas
+app.use("/auth", authRoutes);
+
+// Puerto
+const PORT = process.env.PORT || 3000;
+
+// Servidor
+app.listen(PORT, () => {
+  console.log(`🧍 Auth Service corriendo en el puerto ${PORT}`);
+});
---
+++ Proyect_Microservicios/services/orders-service/src/index.js
@@ -0,0 +1,28 @@
+﻿/**
+ * Orders Service
+ * Gestión de órdenes (creación, consulta, actualización)
+ */
+
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import orderRoutes from "./routes/orders.routes.js";
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+
+connectDB();
+
+app.use("/orders", orderRoutes);
+
+const PORT = process.env.PORT || 3003;
+
+app.listen(PORT, () => {
+  console.log(`🧾 Orders Service corriendo en el puerto ${PORT}`);
+});
